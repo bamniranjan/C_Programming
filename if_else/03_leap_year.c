ab4b7c737f459432b256eb4ecbd681c391baf16a
bamniranjan <bamnirajan577@gmail.com>
2026-03-31 20:54:15 +0545

    add problem 1 to 10

---
+++ if_else/03_leap_year.c
@@ -0,0 +1,6 @@
+// Wap to check whether the given year is leap year or not.
+
+#include<stdio.h>
+int main(){
+    
+}
---
+++ if_else/10_side_of_triagne.c
@@ -0,0 +1,21 @@
+// Wap to take input of three sides of triangle and check whether the triangle is valid or not
+
+
+#include<stdio.h>
+int main(){
+    int side1,side2,side3;
+    printf("Enter the first side of triangle:");
+    scanf("%d",&side1);
+    printf("enter the second side of triangle:");
+    scanf("%d",&side2);
+    printf("Enter the third side of triangle:");
+    scanf("%d",&side3);
+
+    if(side1+side2>side3 && side2+side3>side1 && side1+side3>side2){
+        printf("The triangle is valid.");
+    }else{
+        printf("The triangle is not valid.");
+
+    }
+    return 0;
+}
