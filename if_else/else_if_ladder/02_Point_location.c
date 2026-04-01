fdb4c0d6ee543047ced23f95d1d657b660edf80a
bamniranjan <bamnirajan577@gmail.com>
2026-04-01 09:46:56 +0545

    add 1 and 2 problems

---
+++ if_else/else_if_ladder/02_Point_location.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main(){
+    int x,y;
+    printf("Enter the coordinates of the point:");
+    scanf("%d%d",&x,&y);
+
+    if(x==0 && y==0){
+        printf("The point is at the origin.");
+    }else if(x==0){
+        printf("The point is on the y-axis.");
+    }else if(y==0){
+        printf("The point is on the x-axis.");
+    }else{
+        printf("The point does not lie on any axis.");
+    }
+    return 0;
+}
